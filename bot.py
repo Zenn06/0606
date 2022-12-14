793fec6b94768250d0625273715b2416c9a4f506
kimino <you@example.com>
2022-12-14 21:30:18 +0700

    test

---
+++ bot.py
@@ -0,0 +1,33 @@
+# This example requires the 'message_content' intent.
+
+import discord
+import json
+from fun_class import check__
+class MyClient(discord.Client):
+    global seesion,fun_c
+    seesion = []
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        global seesion
+        print(f'Message from {message.author}: {message.content}')
+        # if(mes)
+        if(message.content == "//"):
+           await message.channel.send("กรุณาพิมพ์จำนวนหินที่ต้องการ")
+        #    seesion[message.author.name] = 1
+           ob = {message.author.id:1}
+           
+        #    print(ob[message.author.id])
+           seesion.append(ob)
+           check__(seesion,message.author.id)
+        #    print()
+
+        # print(message)
+        
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('MTA1MDQwMjI5OTg3ODU5MjU2Mw.GdQL5t.zqbfrzLNuppArENvlUraXpBw0JLMniHkBlthiU')
---
+++ fun_class.py
@@ -0,0 +1,5 @@
+import json
+class check__:
+    def __init__(self,args,id):
+       for x in args:
+        print(x["aaa"])
